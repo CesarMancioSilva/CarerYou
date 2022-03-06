cc5ebcc35316cd8f02dd4af7638fe032285c188e
César Mâncio Silva <cesarmanciosilva@gmail.com>
2022-03-06 18:15:38 -0300

    Administração

---
+++ Site/MVC/View/scripts/adm.js
@@ -0,0 +1,9 @@
+const Content = document.querySelector(".cuidador-content");
+function closeContent(){
+    Content.style.display="none";
+
+}
+function viewContent(){
+    Content.style.display="block";
+
+}
